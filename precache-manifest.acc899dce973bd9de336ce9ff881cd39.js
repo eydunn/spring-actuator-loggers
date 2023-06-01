17987ba79695c7f37fe249ec708d478d13323a68
Eyðun Nielsen <eydun.nielsen@globetracker.com>
2023-06-01 11:11:35 +0100

    Updates

---
+++ precache-manifest.acc899dce973bd9de336ce9ff881cd39.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1f6cedc318b38fe355fe",
+    "url": "/spring-actuator-loggers/static/css/main.ba3ec8ee.chunk.css"
+  },
+  {
+    "revision": "1f6cedc318b38fe355fe",
+    "url": "/spring-actuator-loggers/static/js/main.1f6cedc3.chunk.js"
+  },
+  {
+    "revision": "4587b35573b7dda0a77d",
+    "url": "/spring-actuator-loggers/static/js/1.4587b355.chunk.js"
+  },
+  {
+    "revision": "7308f7879240bd28067e",
+    "url": "/spring-actuator-loggers/static/js/2.7308f787.chunk.js"
+  },
+  {
+    "revision": "6d3b90ea66ccf4edc21d",
+    "url": "/spring-actuator-loggers/static/js/runtime~main.6d3b90ea.js"
+  },
+  {
+    "revision": "7addbda7294c998f995592b66a928cd8",
+    "url": "/spring-actuator-loggers/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/spring-actuator-loggers/precache-manifest.acc899dce973bd9de336ce9ff881cd39.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/spring-actuator-loggers/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
